9106819c554b2d09905b6ff51fb3db29e4f0d7d8
bharathkumar <bharathrallapalli@yahoo.in>
2019-05-29 13:30:11 +0530

    first commit

---
+++ src/src/main/java/net/retail/NetretailApplication.java
@@ -0,0 +1,13 @@
+package net.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetretailApplication.class, args);
+	}
+
+}
